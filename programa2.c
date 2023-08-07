e20b437f289b9c696eb39d274f02857c69068209
Roberto Zarate <2997331650101@ingenieria.usac.edu.gt>
2023-08-06 20:27:38 -0600

    primer commit

---
+++ programa2.c
@@ -0,0 +1,14 @@
+/*Nombre: Julio Roberto Zarate Lopez*/
+/*Carnet: 202300868*/
+/*CUI: 2997331650101*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main() { 
+    double pi= 3.14;
+    double radius = 8.9;
+    double area= pi*radius*radius;
+    system("cls");
+    printf("el valor del Area es %lf \n", area);
+    return 0;
+}
---
+++ progrma1.c
@@ -0,0 +1,12 @@
+/*Nombre: Julio Roberto Zarate Lopez*/
+/*Carnet: 202300868*/
+/*CUI: 2997331650101*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main() { 
+    int Numero=1000;
+    system("cls");
+    printf("%d \n\n", Numero);
+    return 0;
+}
